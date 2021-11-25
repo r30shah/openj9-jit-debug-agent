0bee7816bcd8a5f383587fd9be92412742479c69
Rahil Shah <rahil@ca.ibm.com>
2021-11-25 15:42:48 -0500

    Add JNI Call to set ForceUsePreexistence
    Add a JNI method which can be used to set ForceUsePreexistence JIT
    options for DebugAgent.
    
    Signed-off-by: Rahil Shah <rahil@ca.ibm.com>

--- jcl/src/java.base/share/classes/com/ibm/jit/JITHelpers.java
+++ jcl/src/java.base/share/classes/com/ibm/jit/JITHelpers.java
@@ -1223,6 +1223,8 @@ public final class JITHelpers {
 
 	public static native void dispatchVirtual();
 
+	public static native void setForceUsePreexistence();
+
 	private native static final void debugAgentRun(MethodAccessor ma, Object obj, Object[] args);
 
 	/**
